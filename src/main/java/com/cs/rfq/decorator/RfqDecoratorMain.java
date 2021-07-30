95cd4099c169d089592d914b1fdfc2593823cef2
Liam Lindy <llindy@elon.edu>
2021-07-30 14:53:18 -0400

    updated POM to include the JSON package. RFQ works well.

--- src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
+++ src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
@@ -12,12 +12,22 @@ public class RfqDecoratorMain {
         System.setProperty("spark.master", "local[4]");
 
         //TODO: create a Spark configuration and set a sensible app name
+        SparkConf conf = new SparkConf().setAppName("RfqDecoratorMain");
 
         //TODO: create a Spark streaming context
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
         //TODO: create a Spark session
+        SparkSession sparksession = SparkSession
+                .builder()
+                .appName("RfqDecoratorMain")
+                .config(conf)
+                .getOrCreate();
 
         //TODO: create a new RfqProcessor and set it listening for incoming RFQs
+        RfqProcessor rfqpro = new RfqProcessor(sparksession, jssc);
+        rfqpro.startSocketListener();
     }
-
 }
+
+
