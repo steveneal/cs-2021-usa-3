7654f33c774ad1cf28ecd1cf260432389432d8af
Shambhavi Gupta <shambhaviaug11gupta@gmail.com>
2021-07-30 15:57:29 -0400

    Trade Data Loader

--- src/main/java/com/cs/rfq/decorator/TradeDataLoader.java
+++ src/main/java/com/cs/rfq/decorator/TradeDataLoader.java
@@ -17,14 +17,44 @@ public class TradeDataLoader {
 
     private final static Logger log = LoggerFactory.getLogger(TradeDataLoader.class);
 
+    /*StructType trade_schema = new StructType()
+            .add("TraderID", LongType, true )
+            .add("EntityID", LongType, true  )
+            //.add("MsgType", IntegerType, true )
+            //.add("TradeReportID", LongType, true)
+            //.add("PreviouslyReported", StringType, true)
+            .add("SecurityID", StringType, true)
+            //.add("SecurityIDSource", StringType, true)
+            .add("LastQty", LongType, true)
+            .add("LastPx", DoubleType, true)
+            .add("TradeDate", DateType, true)
+            //.add("TransactTime", StringType, true)
+            //.add("NoSides", IntegerType, true)
+            //.add("Side", IntegerType, true)
+            //.add("OrderID", LongType, true)
+            .add("Currency", StringType, true);
+ */
+
+
     public Dataset<Row> loadTrades(SparkSession session, String path) {
         //TODO: create an explicit schema for the trade data in the JSON files
-        StructType schema = null;
+        StructType schema = new StructType(new StructField[]{
+                new StructField("TraderID", LongType, false, Metadata.empty()),
+                new StructField("EntityID", LongType, false, Metadata.empty()),
+                new StructField("SecurityID", StringType, false, Metadata.empty()),
+                new StructField("LastQty", LongType, false, Metadata.empty()),
+                new StructField("LastPx", DoubleType, false, Metadata.empty()),
+                new StructField("TradeDate", DateType, false, Metadata.empty()),
+                new StructField("Currency", StringType, false, Metadata.empty()),
+
+        });
 
         //TODO: load the trades dataset
-        Dataset<Row> trades = null;
+        Dataset<Row> trades = session.read().schema(schema).json(path);
 
         //TODO: log a message indicating number of records loaded and the schema used
+        System.out.println("Number of trades loaded = " + trades.count());
+        //trades.printSchema();
 
         return trades;
     }
