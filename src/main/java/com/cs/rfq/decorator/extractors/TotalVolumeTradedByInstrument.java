d4af14baafbf21e980541b87cceb3f0bb0257933
Liam Lindy <llindy@elon.edu>
2021-08-02 11:52:59 -0400

    updated field names for seurity ID

--- src/main/java/com/cs/rfq/decorator/extractors/TotalVolumeTradedByInstrument.java
+++ src/main/java/com/cs/rfq/decorator/extractors/TotalVolumeTradedByInstrument.java
@@ -4,12 +4,91 @@ import com.cs.rfq.decorator.Rfq;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.joda.time.DateTime;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class TotalVolumeTradedByInstrument implements RfqMetadataExtractor {
+import static com.cs.rfq.decorator.extractors.RfqMetadataFieldNames.*;
+
+public class TotalVolumeTradedByInstrument implements RfqMetadataExtractor{
+
     @Override
     public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
-        return null;
+
+        // create the time frames
+        long todayMs = DateTime.now().withMillisOfDay(0).getMillis();
+        long pastWeekMs = DateTime.now().withMillis(todayMs).minusWeeks(1).getMillis();
+        long pastYearMs = DateTime.now().withMillis(todayMs).minusYears(1).getMillis();
+
+        // create the query strings
+        String queryToday = String.format("SELECT sum(LastQty) from trade where SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getIsin(),
+                todayMs);
+
+        String queryWeek = String.format("SELECT sum(LastQty) from trade where SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getIsin(),
+                pastWeekMs);
+
+        String queryYear = String.format("SELECT sum(LastQty) from trade where SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getIsin(),
+                pastYearMs);
+
+        // execute and store the queries
+        trades.createOrReplaceTempView("tradeToday");
+        Dataset<Row> sqlQueryResultsToday = session.sql(queryToday);
+
+        trades.createOrReplaceTempView("tradeWeek");
+        Dataset<Row> sqlQueryResultsWeek = session.sql(queryWeek);
+
+        trades.createOrReplaceTempView("tradeYear");
+        Dataset<Row> sqlQueryResultsYear = session.sql(queryYear);
+
+        // calculate the volume
+        Object volumeToday = sqlQueryResultsToday.first().get(0);
+        if (volumeToday == null) {
+            volumeToday = 0L;
+        }
+
+        Object volumeWeek = sqlQueryResultsWeek.first().get(0);
+        if (volumeWeek == null) {
+            volumeWeek = 0L;
+        }
+
+        Object volumeYear = sqlQueryResultsYear.first().get(0);
+        if (volumeYear == null) {
+            volumeYear = 0L;
+        }
+
+// create a results hashmap and store results
+        Map<RfqMetadataFieldNames, Object> results = new HashMap<>();
+        results.put(RfqMetadataFieldNames.tradesBySecurityToday, volumeToday);
+        results.put(RfqMetadataFieldNames.tradesBySecurityPastWeek, volumeWeek);
+        results.put(RfqMetadataFieldNames.tradesBySecurityPastYear, volumeYear);
+        return results;
     }
+
+
+
+//    @Override
+//    public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
+//
+//        long todayMs = DateTime.now().withMillisOfDay(0).getMillis();
+//        long pastWeekMs = DateTime.now().withMillis(todayMs).minusWeeks(1).getMillis();
+//        long pastYearMs = DateTime.now().withMillis(todayMs).minusYears(1).getMillis();
+//
+//        Dataset<Row> filtered = trades
+//                .filter(trades.col("SecurityId").equalTo(rfq.getIsin()));
+//
+//        long tradesToday = filtered.filter(trades.col("TradeDate").$greater$eq(new java.sql.Date(todayMs))).count();
+//        long tradesPastWeek = filtered.filter(trades.col("TradeDate").$greater$eq(new java.sql.Date(pastWeekMs))).count();
+//        long tradesPastYear = filtered.filter(trades.col("TradeDate").$greater$eq(new java.sql.Date(pastYearMs))).count();
+//
+//        Map<RfqMetadataFieldNames, Object> results = new HashMap<>();
+//        results.put(tradesWithEntityToday, tradesToday);
+//        results.put(tradesWithEntityPastWeek, tradesPastWeek);
+//        results.put(tradesWithEntityPastYear, tradesPastYear);
+//        return results;
+//    }
+
 }
--- src/main/java/com/cs/rfq/decorator/extractors/TotalVolumeTradedByLegalEntity.java
+++ src/main/java/com/cs/rfq/decorator/extractors/TotalVolumeTradedByLegalEntity.java
@@ -11,27 +11,63 @@ import java.util.Map;
 
 import static com.cs.rfq.decorator.extractors.RfqMetadataFieldNames.*;
 
-public class TotalVolumeTradedByLegalEntity implements RfqMetadataExtractor{
+public class TotalVolumeTradedByLegalEntity implements RfqMetadataExtractor {
+
     @Override
     public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
 
+        // create the time frames
         long todayMs = DateTime.now().withMillisOfDay(0).getMillis();
         long pastWeekMs = DateTime.now().withMillis(todayMs).minusWeeks(1).getMillis();
         long pastYearMs = DateTime.now().withMillis(todayMs).minusYears(1).getMillis();
 
-        Dataset<Row> filtered = trades
-                .filter(trades.col("SecurityId").equalTo(rfq.getIsin()))
-                .filter(trades.col("EntityId").equalTo(rfq.getEntityId()));
+        // create the query strings
+        String queryToday = String.format("SELECT sum(LastQty) from trade where EntityId='%s' AND SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getEntityId(),
+                rfq.getIsin(),
+                todayMs);
 
-        long tradesToday = filtered.filter(trades.col("TradeDate").$greater$eq(new java.sql.Date(todayMs))).count();
-        long tradesPastWeek = filtered.filter(trades.col("TradeDate").$greater$eq(new java.sql.Date(pastWeekMs))).count();
-        long tradesPastYear = filtered.filter(trades.col("TradeDate").$greater$eq(new java.sql.Date(pastYearMs))).count();
+        String queryWeek = String.format("SELECT sum(LastQty) from trade where EntityId='%s' AND SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getEntityId(),
+                rfq.getIsin(),
+                pastWeekMs);
 
+        String queryYear = String.format("SELECT sum(LastQty) from trade where EntityId='%s' AND SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getEntityId(),
+                rfq.getIsin(),
+                pastYearMs);
+
+        // execute and store the queries
+        trades.createOrReplaceTempView("tradeToday");
+        Dataset<Row> sqlQueryResultsToday = session.sql(queryToday);
+
+        trades.createOrReplaceTempView("tradeWeek");
+        Dataset<Row> sqlQueryResultsWeek = session.sql(queryWeek);
+
+        trades.createOrReplaceTempView("tradeYear");
+        Dataset<Row> sqlQueryResultsYear = session.sql(queryYear);
+
+        // calculate the volume
+        Object volumeToday = sqlQueryResultsToday.first().get(0);
+        if (volumeToday == null) {
+            volumeToday = 0L;
+        }
+
+        Object volumeWeek = sqlQueryResultsWeek.first().get(0);
+        if (volumeWeek == null) {
+            volumeWeek = 0L;
+        }
+
+        Object volumeYear = sqlQueryResultsYear.first().get(0);
+        if (volumeYear == null) {
+            volumeYear = 0L;
+        }
+
+// create a results hashmap and store results
         Map<RfqMetadataFieldNames, Object> results = new HashMap<>();
-        results.put(tradesWithEntityToday, tradesToday);
-        results.put(tradesWithEntityPastWeek, tradesPastWeek);
-        results.put(tradesWithEntityPastYear, tradesPastYear);
+        results.put(RfqMetadataFieldNames.tradesBySecurityToday, volumeToday);
+        results.put(RfqMetadataFieldNames.tradesBySecurityPastWeek, volumeWeek);
+        results.put(RfqMetadataFieldNames.tradesBySecurityPastYear, volumeYear);
         return results;
     }
-
 }
